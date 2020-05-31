28f4960edf12980374cddbf604956debee2357e2
Luis Arias <kaaloo@gmail.com>
2020-05-31 18:59:11 +0200

    Override GPU detection to run locally

--- dueling_dqn_agent.py
+++ dueling_dqn_agent.py
@@ -14,8 +14,10 @@ GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate
 UPDATE_EVERY = 4        # how often to update the network
+USE_GPU = False         # override GPU detection
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device(
+    "cuda:0" if USE_GPU and torch.cuda.is_available() else "cpu")
 print("Torch device: {}".format(device))
 
 
